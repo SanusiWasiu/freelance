dad0674f6f7ad468588d045746c7b2c2b920165c
SanusiWasiu <was.sanusi@gmail.com>
2022-10-26 23:46:23 +0100

    adjust the site

--- js/index.js
+++ js/index.js
@@ -12,20 +12,30 @@ function currentDiv(n) {
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("customer-profile");
-    // var y = document.getElementById("container")
 
-    // x.animate({
-    //     top: -y.height() * x.index()
-    // }, 450);
-//   var dots = document.getElementsByClassName("demo"); 
     if (n > x.length) {slideIndex = 1}    
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
     }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" w3-red", "");
-//   }
+
     x[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " w3-red";
+}
+
+function scrollFunction1() {
+    let e = document.getElementById("about-me");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'start'
+    });
+}
+
+function scrollFunction2() {
+    let e = document.getElementById("reviews");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'start'
+    });
 }
